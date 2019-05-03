cf60d0db5610cd0f04531b2269a9585b52d91423
RaulGrosmam <49730433+RaulGrosmam@users.noreply.github.com>
2019-05-03 09:20:29 -0300

    Update loginsteps.py

--- TentativaDeLogin/loginsteps.py
+++ TentativaDeLogin/loginsteps.py
@@ -2,6 +2,7 @@
 def step_impl(context):
     context.ObjLogin.home_extra()
     context.ObjLogin.url_check()
+    assert 'Extra.com.br: o site da família e a maior loja de Informática do Brasil' in context.driver.title
 
 @when(u'é clicado em Entre ou Cadastre-se')
 def step_impl(context):
@@ -13,14 +14,16 @@ def step_impl(context):
 
 @then(u'A Tela de login aparece')
 def step_impl(context):
-     context.ObjLogin.url_check()
+     assert context.driver.page_source.find('https://carrinho.extra.com.br/Checkout?ReturnUrl=https://www.extra.com.br#login')
 
 @when(u'serão insiridas as credenciais do site')
 def step_impl(context):
      context.ObjLogin.email()
      context.ObjLogin.senha()
      context.ObjLogin.atraso()
+     assert context.driver.page_source.find('opacity: 0.5;c')
      context.ObjLogin.botao_continuar()
+
 
 @then(u'O Captcha aparecerá')
 def step_impl(context):
